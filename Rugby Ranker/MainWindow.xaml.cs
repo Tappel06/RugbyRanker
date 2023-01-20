d550ddd137193832572c134c149ad42f79c284eb
Tappel06 <122260883+Tappel06@users.noreply.github.com>
2023-01-20 20:33:12 +0200

    stream write reader update
    This update contains an upadate on fixing the comma issue with stream write and reader. comma is replaced with modulo sign. also teams have been added to the database

---
+++ Rugby Ranker/MainWindow.xaml.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rugby_Ranker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ProgramMethods.CreateTeams();
+            RetreiveData.Retreive();
+            RatingSystem.CalculateRecords();
+            ShowMainMenuOptions();
+            HideManageTeamsOptions();
+            HideUpdateMatchUpdateOptions();
+            HideTeamsOptions();
+            HideAddTeamOptions();
+            HideRemoveTeamOptions();
+            HideAddMatchGrid();
+            HideHistoryGrid();
+            for (int i = 0; i < ProgramMethods.RugbyTeams.Length; i++)
+            {
+                if (ProgramMethods.RugbyTeams[i].GetIsTeamAccountActivated() == true)
+                {
+                    Console.WriteLine(ProgramMethods.RugbyTeams[i].GetTeamName().ToString() + ": R = " + ProgramMethods.RugbyTeams[i].GetRating().ToString() + " CR = " + Convert.ToDouble(ProgramMethods.RugbyTeams[i].GetCalculatedRating()) );
+                }
+            }
+            for (int i = 0; i <ProgramMethods.TeamDescendingOrderString.Count; i++) 
+            {
+                Console.WriteLine(ProgramMethods.TeamDescendingOrderString[i].ToString());
+            }
+        }
+
+        private void UpdateListBoxes() 
+        {
+            RemoveTeamsListBox.Items.Clear();
+            Home_Team_Combobox_addMatch.Items.Clear();
+            Away_Team_Combobox_addMatch.Items.Clear();
+
+            for (int i = 0; i < ProgramMethods.RugbyTeams.Length; i++) 
+            {
+                if (ProgramMethods.RugbyTeams[i].GetIsTeamAccountActivated() == true) 
+                {
+                    RemoveTeamsListBox.Items.Add(ProgramMethods.RugbyTeams[i].GetTeamName());
+                    Home_Team_Combobox_addMatch.Items.Add(ProgramMethods.RugbyTeams[i].GetTeamName());
+                    Away_Team_Combobox_addMatch.Items.Add(ProgramMethods.RugbyTeams[i].GetTeamName());
+                }
+            }
+
+            if (MatchDatabase.CountTotalRecords() > 0)
+            {
+                HistoryListBox.Items.Clear();
+                for (int i = 0; i < MatchDatabase.CountTotalRecords(); i++)
+                {
+                    HistoryListBox.Items.Add(MatchDatabase.GetFullRecord(i));
+                }
+            }
+            else 
+            {
+                HistoryListBox.Items.Clear();
+            }
+        }
+
+        private void ShowMainMenuOptions()
+        {
+            MainMenuSelectedGrid.Visibility = Visibility.Visible;
+            ProgramMethods.BuildMainMenuTeamsDisplay();
+            setupmainMenuDisplay();
+        }
+
+        private void setupmainMenuDisplay() 
+        {
+           
+            mainMenuListbox.Items.Clear();
+
+            for (int i = 0; i < ProgramMethods.TeamDescendingOrderString.Count; i++)
+            {
+                mainMenuListbox.Items.Add(ProgramMethods.TeamDescendingOrderString[i].ToString());
+            }
+        }
+
+        private void HideMainMenuOptions()
+        {
+            MainMenuSelectedGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowManageTeamsOptions()
+        {
+            ManageTeamsGrid.Visibility = Visibility.Visible;
+        }
+
+        private void HideManageTeamsOptions()
+        {
+            ManageTeamsGrid.Visibility = Visibility.Collapsed;
+        }
+
+        //Match updates
+        private void ShowUpdateMatchUpdateOptions()
+        {
+            MatchUpdateGrid.Visibility = Visibility.Visible;
+        }
+
+        private void HideUpdateMatchUpdateOptions()
+        {
+            MatchUpdateGrid.Visibility = Visibility.Collapsed;
+        }
+
+        //Show Add Matches
+        private void ShowAddMatchGrid() 
+        {
+            AddMatchGrid.Visibility= Visibility.Visible;
+        }
+
+        private void HideAddMatchGrid() 
+        {
+            AddMatchGrid.Visibility = Visibility.Collapsed;
+        }
+
+        //Show History
+        private void ShowHistoryGrid() 
+        {
+            History_Grid.Visibility= Visibility.Visible;
+        }
+
+        private void HideHistoryGrid() 
+        {
+            History_Grid.Visibility = Visibility.Collapsed;
+        }
+
+        //Show Edit Team Records
+        private void ShowEditTeamsGrid() 
+        {
+            EditTeamsGrid.Visibility= Visibility.Visible;
+        }
+
+        private void HideEditTeamsGrid() 
+        {
+            EditTeamsGrid.Visibility = Visibility.Collapsed;
+        }
+
+        //Teams options
+        private void ShowTeamsOptions()
+        {
+            TeamsGrid.Visibility = Visibility.Visible;
+        }
+
+        //Add Teams
+        private void HideTeamsOptions()
+        {
+            TeamsGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowAddTeamOptions()
+        {
+            AddTeamGrid.Visibility = Visibility.Visible;
+        }
+
+        private void HideAddTeamOptions()
+        {
+            AddTeamGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddTeamFromManageTeams() 
+        { 
+            ProgramMethods.AddNewTeam(TeamNameTextBox.Text, Convert.ToDouble(DefaultRatingTextBox.Text));
+            RemoveTeamsListBox.Items.Add(TeamNameTextBox.Text);
+        }
+
+        //Remove Teams
+        private void ShowRemoveTeamOptions() 
+        {
+            ManageTeamsRemoveTeamGrid.Visibility = Visibility.Visible;
+            UpdateListBoxes();
+        }
+
+        private void HideRemoveTeamOptions() 
+        {
+            ManageTeamsRemoveTeamGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void RemoveTeamFromManagedTeams() 
+        {
+            for(int i = 0; i < ProgramMethods.RugbyTeams.Length; i++) 
+            {
+                if (ProgramMethods.RugbyTeams[i].GetTeamName() != null)
+                {
+                    if (ProgramMethods.RugbyTeams[i].GetTeamName() == RemoveTeamsListBox.SelectedValue.ToString())
+                    {
+                        ProgramMethods.RemoveTeam(RemoveTeamsListBox.SelectedValue.ToString());
+                        RemoveTeamsListBox.Items.Remove(RemoveTeamsListBox.SelectedItem);
+                        break;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Failed to match strings");
+                    }
+                }
+            }
+        }
+
+        private void RemoveAllTeamsFromManagedTeams() 
+        {
+            for(int i = 0; i < ProgramMethods.RugbyTeams.Length; i++) 
+            {
+                ProgramMethods.RugbyTeams[i].SetTeamName(null);
+                ProgramMethods.RugbyTeams[i].SetRating(0);
+                ProgramMethods.RugbyTeams[i].DeactivateTeamAccount();
+            }
+            RemoveTeamsListBox.Items.Clear();
+        }
+
+        private void AddNewRecord() 
+        {
+            
+            string homeTeam = Home_Team_Combobox_addMatch.SelectedItem.ToString();
+            int homeScore =  Convert.ToInt32(home_score_textbox_addMatch.Text);
+            int awayScoreScore = Convert.ToInt32(away_score_textbox_addMatch.Text);
+            string awayTeam = Away_Team_Combobox_addMatch.SelectedItem.ToString();
+            int year = Convert.ToInt32(YearTextBoxAddRecord.Text);
+            int month = Convert.ToInt32(MonthTextBoxAddRecord.Text);
+            int day = Convert.ToInt32(DayTextBoxAddRecord.Text);
+            MatchDatabase.AddRecord(homeTeam, homeScore, awayTeam, awayScoreScore, year, month, day);
+        }
+
+        //Highlighted buttons
+        private void MainMenuButtonHighlighted()
+        {
+            MainMenuButton.Height = 50;
+            EditTeamButton.Margin = new Thickness (10,115,0,0);
+            MatchUpdatesButton.Margin = new Thickness(10,140,0,0);
+            TeamsButton.Margin = new Thickness(10, 165, 0, 0);
+        }
+
+        private void MainMenuButtonNotHighlighted() 
+        {
+            MainMenuButton.Height = 20;
+            EditTeamButton.Margin = new Thickness(10, 85, 0, 0);
+            MatchUpdatesButton.Margin = new Thickness(10, 110, 0, 0);
+            TeamsButton.Margin = new Thickness(10, 135, 0, 0);
+        }
+
+        private void EditTeamButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EditTeamButton.Height = 50;
+            MatchUpdatesButton.Margin = new Thickness(10, 140, 0, 0);
+            TeamsButton.Margin = new Thickness(10, 165, 0, 0);
+        }
+
+        private void EditTeamButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            EditTeamButton.Height = 20;
+            MatchUpdatesButton.Margin = new Thickness(10, 110, 0, 0);
+            TeamsButton.Margin = new Thickness(10, 135, 0, 0);
+        }
+
+        private void MatchUpdatesButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MatchUpdatesButton.Height = 50;
+            TeamsButton.Margin = new Thickness(10, 165, 0, 0);
+        }
+
+        private void MatchUpdatesButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MatchUpdatesButton.Height = 20;
+            TeamsButton.Margin = new Thickness(10, 135, 0, 0);
+        }
+
+        private void TeamsButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TeamsButton.Height = 50;
+        }
+
+        private void TeamsButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            TeamsButton.Height = 20;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Main_Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void EditTeamButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuHeader.Content = "Team Management";
+            MainMenuButton.IsEnabled = true;
+            EditTeamButton.IsEnabled = false;
+            MatchUpdatesButton.IsEnabled = true;
+            TeamsButton.IsEnabled = true;
+
+            HideMainMenuOptions();
+            ShowManageTeamsOptions();
+            HideUpdateMatchUpdateOptions();
+            HideTeamsOptions();
+        }
+
+        private void MatchUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuHeader.Content = "Update Matches";
+            MainMenuButton.IsEnabled = true;
+            EditTeamButton.IsEnabled = true;
+            MatchUpdatesButton.IsEnabled = false;
+            TeamsButton.IsEnabled = true;
+
+            HideMainMenuOptions();
+            HideManageTeamsOptions();
+            ShowUpdateMatchUpdateOptions();
+            HideTeamsOptions();
+            HideEditTeamsGrid();
+
+            UpdateListBoxes();
+        }
+
+        private void TeamsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuHeader.Content = "Teams";
+            MainMenuButton.IsEnabled = true;
+            EditTeamButton.IsEnabled = true;
+            MatchUpdatesButton.IsEnabled = true;
+            TeamsButton.IsEnabled = false;
+
+            HideMainMenuOptions();
+            HideManageTeamsOptions();
+            HideUpdateMatchUpdateOptions();
+            ShowTeamsOptions();
+        }
+
+        private void MainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuHeader.Content = "Main Menu";
+            MainMenuButton.IsEnabled = false;
+            EditTeamButton.IsEnabled = true;
+            MatchUpdatesButton.IsEnabled = true;
+            TeamsButton.IsEnabled = true;
+
+            ShowMainMenuOptions();
+            HideManageTeamsOptions();
+            HideUpdateMatchUpdateOptions();
+            HideTeamsOptions();
+        }
+
+        private void MainMenuButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainMenuButtonHighlighted();
+        }
+
+        private void MainMenuButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MainMenuButtonNotHighlighted();
+        }
+
+        private void ManageTeamsAddTeam_Click(object sender, RoutedEventArgs e)
+        {
+            ManageTeamsAddTeam.IsEnabled = false;
+            ManageTeamsRemoveTeam.IsEnabled = true;
+
+            ShowAddTeamOptions();
+            HideRemoveTeamOptions();
+            HideEditTeamsGrid();
+        }
+
+        private void ManageTeamsRemoveTeam_Click(object sender, RoutedEventArgs e)
+        {
+            ManageTeamsAddTeam.IsEnabled = true;
+            ManageTeamsRemoveTeam.IsEnabled = false;
+
+            HideAddTeamOptions();
+            ShowRemoveTeamOptions();
+        }
+
+        private void AddTeamGridAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddTeamFromManageTeams();
+            StoreData.StoreProgramData();
+        }
+
+        private void ManageTeamsRemoveTeamsRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RemoveTeamsListBox.SelectedItem != null) 
+            {
+                RemoveTeamFromManagedTeams();
+            }
+            StoreData.StoreProgramData();
+        }
+
+        private void ManageTeamsRemoveAllTeamsButton_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveAllTeamsFromManagedTeams();
+            StoreData.StoreProgramData();
+        }
+
+        private void AddMatchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAddMatchGrid();
+            HideHistoryGrid();
+            HideEditTeamsGrid();
+
+            AddMatchButton.IsEnabled = false;
+            HistoryButton.IsEnabled = true;
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            HideAddMatchGrid();
+            ShowHistoryGrid();
+            HideEditTeamsGrid();
+
+            AddMatchButton.IsEnabled = true;
+            HistoryButton.IsEnabled = false;
+
+            UpdateListBoxes();
+        }
+
+        private void HistoryEditMatchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowEditTeamsGrid();
+            StoreData.StoreProgramData();
+        }
+
+        private void AddMatchSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewRecord();
+            RatingSystem.CalculateRecords();
+            StoreData.StoreProgramData();
+        }
+
+        private void HistoryDeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            MatchDatabase.RemoveRecord(Convert.ToInt32(HistoryListBox.SelectedIndex));
+            UpdateListBoxes();
+            RatingSystem.CalculateRecords();
+            StoreData.StoreProgramData();
+        }
+    }
+}
---
+++ Rugby Ranker/MatchDatabase.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rugby_Ranker
+{
+    internal class MatchDatabase
+    {
+        private static ArrayList HomeTeamName = new ArrayList();
+        private static ArrayList HomeTeamScore = new ArrayList();
+        private static ArrayList AwayTeamName = new ArrayList();
+        private static ArrayList AwayTeamScore = new ArrayList();
+        private static ArrayList MatchDate = new ArrayList();
+        private static ArrayList HashID = new ArrayList();
+
+        //Home Team Name Properties
+        public static string GetHomeTeamName(int index)
+        {
+                return HomeTeamName[index].ToString();
+        }
+
+        public static void AddHomeTeamName(string name)
+        {
+            HomeTeamName.Add(name);
+        }
+
+        private static void RemoveHomeTeamName(int index)
+        {
+            HomeTeamName.RemoveAt(index);
+        }
+
+        //Home team score properties
+        public static int GetHomeTeamScore(int index)
+        {
+            return (int)HomeTeamScore[index];
+        }
+
+        public static void AddHomeTeamScore(int score)
+        {
+            HomeTeamScore.Add(score);
+        }
+
+        private static void RemoveHomeTeamScore(int index)
+        {
+            HomeTeamScore.RemoveAt(index);
+        }
+
+
+        //Away Team Name Properties
+        public static string GetAwayTeamName(int index)
+        {
+            return AwayTeamName[index].ToString();
+        }
+
+        public static void AddAwayTeamName(string name)
+        {
+            AwayTeamName.Add(name);
+        }
+
+        private static void RemoveAwayTeamName(int index)
+        {
+            AwayTeamName.RemoveAt(index);
+        }
+
+        //Away Team Score Properties
+        public static int GetAwayTeamScore(int index)
+        {
+            return (int)AwayTeamScore[index];
+        }
+
+        public static void AddAwayTeamScore(int score)
+        {
+            AwayTeamScore.Add(score);
+        }
+
+        private static void RemoveAwayTeamScore(int index)
+        {
+            AwayTeamScore.RemoveAt(index);
+        }
+
+        //MatchDate Properties
+        public static DateTime GetMatchDate(int index)
+        {
+            return Convert.ToDateTime(MatchDate[index]);
+        }
+
+        public static void AddMatchDayByString(string a) 
+        {
+            DateTime date = DateTime.ParseExact(a, "yyyy/MM/dd", System.Globalization.CultureInfo.InvariantCulture);
+            MatchDate.Add(date);
+        }
+
+        private static void AddMatchDate(int year, int month, int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            MatchDate.Add(date);
+        }
+
+        private static void RemoveMatchDate(int index)
+        {
+            MatchDate.RemoveAt(index);
+        }
+
+
+        //HashID Properties
+        public static string GetHashID(int index)
+        {
+            return HashID[index].ToString();
+        }
+
+        public static void AddHashIDByString(string a) 
+        {
+            HashID.Add(a);
+        }
+
+        private static void AddHashID()
+        {
+            HashID.Add(GenerateHashID());
+        }
+
+        private static void RemoveHashID(int index)
+        {
+            HashID.RemoveAt(index);
+        }
+
+        //Generate a unique ID by  using HashCode
+        private static string GenerateHashID()
+        {
+            string buildString;
+            string hashID;
+            int index = HomeTeamName.Count - 1;
+            buildString = HomeTeamName[index].ToString() + AwayTeamName[index].ToString();
+            hashID = buildString.GetHashCode().ToString();
+            return hashID;
+        }
+
+        //Add a record
+        public static void AddRecord(string homeName, int homeScore, string awayName, int awayScore, int year, int month, int day)
+        {
+            AddHomeTeamName(homeName);
+            AddHomeTeamScore(homeScore);
+            AddAwayTeamName(awayName);
+            AddAwayTeamScore(awayScore);
+            AddMatchDate(year, month, day);
+            AddHashID();
+        }
+
+        public static void RemoveRecord(int index)
+        {
+            RemoveHomeTeamName(index);
+            RemoveHomeTeamScore(index);
+            RemoveAwayTeamName(index);
+            RemoveAwayTeamScore(index);
+            RemoveMatchDate(index);
+            RemoveHashID(index);
+        }
+
+        public static void RemoveAllRecords()
+        {
+            HomeTeamName.Clear();
+            HomeTeamScore.Clear();
+            AwayTeamName.Clear();
+            AwayTeamScore.Clear();
+            MatchDate.Clear();
+            HashID.Clear();
+        }
+
+        public static string GetFullRecord(int index)
+        {
+            if(HomeTeamName.Count > 0) 
+            {
+                string date = GetMatchDate(index).ToShortDateString();
+                string home = GetHomeTeamName(index);
+                string hS = GetHomeTeamScore(index).ToString();
+                string away = GetAwayTeamName(index);
+                string aS = GetAwayTeamScore(index).ToString();
+                string ID = GetHashID(index);
+
+                string fullRecord = (index + ". " + date + ". " + home + ": " + hS + " VS " + away + ": " + aS + ". ID: " + ID);
+
+                return fullRecord;
+            }
+            else 
+            {
+                return null;
+            }
+        }
+
+        public static int CountTotalRecords() 
+        {
+            int total;
+
+            if (HomeTeamName.Count > 0) 
+            {
+                total = HomeTeamName.Count;
+            }
+            else 
+            {
+                total = 0;
+            }
+            return total;
+        }
+    }
+}
